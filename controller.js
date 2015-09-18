07be6ee3cea8bcc7eba93348f7120f1ffb5e79d4
Wade Mauger <wadeanthony0100@gmail.com>
2015-09-18 02:15:14 -0400

    Continued improvement of data scraping, initial implementation of popup.

--- controller.js
+++ controller.js
@@ -4,6 +4,11 @@
  * hours and pay.
 */
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 var timestamps = [];
 var earnings = [];
 
@@ -28,7 +33,11 @@ function get_records(TL, EL){
 }
 
 
-function load_next_job(){
+function get_number_of_jobs(){
+    /*
+    * return the number of jobs listed on the Timecard
+    * */
+
     //get all radio buttons on the page
     var jobs_rough = document.getElementsByTagName("md-radio-button");
     var jobs = [];
@@ -39,12 +48,75 @@ function load_next_job(){
             jobs.push(jobs_rough[x]);
         }
     }
+    return jobs.length;
+}
+
+function load_next_job(){
+    //get all radio buttons on the page
+    var jobs_rough = document.getElementsByTagName("md-radio-button");
+    var jobs = [];
+    var curr_job;
+
+    //isolate those whose labels end in "hr" (jobs should end in hourly rates per /hr)
+    for(var x=0 ; x<jobs_rough.length ; x++){
+        if (jobs_rough[x].innerText.slice(-2) == "hr"){
+            jobs.push(jobs_rough[x]);
+        }
+    }
+
+    //find the currently selected job, and select the next one
+    for(var x=0 ; x<jobs.length-1 ; x++){
+        if(jobs[x].getAttribute("aria-checked") == "true"){
+            jobs[x+1].click();
+            return;
+        }
+    }
 
 }
+
+
+function sum_time(){
+    var sum = 0;
+    for (var time in timestamps){
+        var min = parseInt(timestamps[time].slice(-2));
+        var hr = 0;
+        if(timestamps[time].length <= 4){
+            hr = parseInt(timestamps[time].slice(0,1));
+        }else{
+            hr = parseInt(timestamps[time].slice(0,2));
+        }
+        console.log(hr, min);
+        sum += hr*3600000;
+        sum += min*60000;
+    }
+    return sum;
+}
+
+
+function sum_wages(){
+    for(var payment in earnings){
+        var sum = 0.0;
+        sum += parseFloat(earnings[payment].slice(1));
+        console.log("wages sum is " +sum);
+    }
+    return sum;
+}
+
 
 function main() {
-    //window.setTimeout("get_records(timestamps, earnings)", 1500);
-    load_next_job();
+    var num_jobs = get_number_of_jobs();
+    var wait_time = 1000;
+    get_records(timestamps, earnings);
+    for(var x=1 ; x<num_jobs ; x++){
+        window.setTimeout("load_next_job()", wait_time);
+        wait_time += 500;
+        window.setTimeout("get_records(timestamps, earnings)",wait_time);
+        console.log(wait_time)
+    }
+    window.setTimeout('document.addEventListener("hello", function(data) {chrome.runtime.sendMessage([sum_time(), sum_wages()]);}); var event = document.createEvent("Event"); event.initEvent("hello"); document.dispatchEvent(event);', 2000);//(sum_time(), sum_wages())
 }
 
-window.setTimeout("main()", 1500);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log(message);
+    window.setTimeout("main()", 1500);
+});
