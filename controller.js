69ac5367168ff9749efe503b850c757c2a19e38b
Wade Mauger <wadeanthony0100@gmail.com>
2015-09-17 22:31:20 -0400

    Continue implementation of get_records function, initial implementation of load_next_job function

--- controller.js
+++ controller.js
@@ -1,21 +1,50 @@
 /**
  * Created by Wade on 9/17/2015.
- */
+ * Scrape the Timecard website for information about
+ * hours and pay.
+*/
 
-function get_time_one(){
+var timestamps = [];
+var earnings = [];
+
+function get_records(TL, EL){
+    /*
+    * Add the time worked and the wages earned
+    * from the job currently displayed to the lists
+    * of wages.
+    * */
     var rows = document.getElementsByTagName("td");
-    console.log(rows);
     var seen_blank_row=false;
-    var time_stamp;
-    var earning_stamp;
     for(var x=0; x<rows.length ; x++ ){
         if(seen_blank_row){
-            //TODO assign values
-            return (time_stamp, earning_stamp);
+            TL.push(rows[x].innerHTML);
+            EL.push(rows[x+1].innerHTML);
+            return;
         }
         if(rows[x].innerHTML.length<1){
             seen_blank_row=true;
         }
     }
 }
-get_time_one();
+
+
+function load_next_job(){
+    //get all radio buttons on the page
+    var jobs_rough = document.getElementsByTagName("md-radio-button");
+    var jobs = [];
+
+    //isolate those whose labels end in "hr" (jobs should end in hourly rates per /hr)
+    for(var x=0 ; x<jobs_rough.length ; x++){
+        if (jobs_rough[x].innerText.slice(-2) == "hr"){
+            jobs.push(jobs_rough[x]);
+        }
+    }
+
+}
+
+function main() {
+    //window.setTimeout("get_records(timestamps, earnings)", 1500);
+    load_next_job();
+}
+
+window.setTimeout("main()", 1500);
